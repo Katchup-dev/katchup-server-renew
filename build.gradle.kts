466f27f287ebb973721138c398bc9c3cbde3079a
unanchoi <unanc2@gmail.com>
2024-04-16 01:01:15 +0900

    setting: jwt, kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,9 +48,17 @@ dependencies {
     // H2 Database
     runtimeOnly("com.h2database:h2")
 
+    // JWT
+    implementation("com.auth0:java-jwt:4.4.0")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Kotest
+    val kotestVersion = "5.8.1"
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 
     val kotestSpringExtensionVersion = "1.1.3"
     testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestSpringExtensionVersion")
