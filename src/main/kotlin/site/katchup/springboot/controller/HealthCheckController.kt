7eadd333a445c096cc863499c8cdd0a13619b7a7
unanchoi <unanc2@gmail.com>
2024-04-16 01:05:48 +0900

    refactor: BaseResponse로 health check 리팩토링

--- src/main/kotlin/site/katchup/springboot/controller/HealthCheckController.kt
+++ src/main/kotlin/site/katchup/springboot/controller/HealthCheckController.kt
@@ -1,13 +1,16 @@
 package site.katchup.springboot.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import site.katchup.springboot.global.message.SuccessMessage
+import site.katchup.springboot.global.response.BaseResponse
 
 @RestController
 class HealthCheckController {
 
     @GetMapping("/health")
-    fun healthCheck(): String {
-        return "OK"
+    fun healthCheck(): ResponseEntity<BaseResponse<String>> {
+        return BaseResponse.ok(SuccessMessage.SUCCESS_HEALTH_CHECK, "OK")
     }
 }
