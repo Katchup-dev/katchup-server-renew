91657f6f6e88e2125a5d1fe7bd3f7cffb0e2599b
unanchoi <unanc2@gmail.com>
2024-04-24 23:26:54 +0900

    feat: set up FailMessage about token

--- src/main/kotlin/site/katchup/springboot/global/message/FailMessage.kt
+++ src/main/kotlin/site/katchup/springboot/global/message/FailMessage.kt
@@ -2,4 +2,15 @@ package site.katchup.springboot.global.message
 
 enum class FailMessage(
     val value: String,
-)
+) {
+    /*
+        * 401 UNAUTHORIZED
+     */
+    INVALID_TOKEN("유효하지 않은 토큰"),
+
+    /*
+        * 500 INTERNAL_SERVER_ERROR
+    */
+    INTERNAL_SERVER_ERROR("서버 내부 오류"),
+    ;
+}
