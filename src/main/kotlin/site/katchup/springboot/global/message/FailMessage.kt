2680938af7a739bf605464ac969b96dbc5e9d1d7
unanchoi <unanc2@gmail.com>
2024-05-02 22:06:18 +0900

    feat: 응답 메세지 추가

--- src/main/kotlin/site/katchup/springboot/global/message/FailMessage.kt
+++ src/main/kotlin/site/katchup/springboot/global/message/FailMessage.kt
@@ -9,6 +9,11 @@ enum class FailMessage(
     INVALID_TOKEN("유효하지 않은 토큰"),
 
     /*
+        * 404 NOT FOUND
+    */
+    MEMBER_NOT_FOUND("해당 사용자를 찾을 수 없음"),
+
+    /*
         * 500 INTERNAL_SERVER_ERROR
     */
     INTERNAL_SERVER_ERROR("서버 내부 오류"),
--- src/main/kotlin/site/katchup/springboot/global/message/SuccessMessage.kt
+++ src/main/kotlin/site/katchup/springboot/global/message/SuccessMessage.kt
@@ -4,5 +4,6 @@ enum class SuccessMessage(
     val value: String,
 ) {
     SUCCESS_HEALTH_CHECK("OK"),
+    SUCCESS_GET_MEMBER("사용자 정보 조회 성공"),
     ;
 }
