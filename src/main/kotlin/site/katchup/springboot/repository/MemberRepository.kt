3a08107a82fd3ecfe28ca5fecdd96c729b67f8b4
unanchoi <unanc2@gmail.com>
2024-05-03 18:44:15 +0900

    feat: 사용자 정보 업데이트 API 구현

--- src/main/kotlin/site/katchup/springboot/repository/MemberRepository.kt
+++ src/main/kotlin/site/katchup/springboot/repository/MemberRepository.kt
@@ -2,12 +2,5 @@ package site.katchup.springboot.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import site.katchup.springboot.entity.Member
-import site.katchup.springboot.exception.entity.EntityNotFoundException
-import site.katchup.springboot.global.message.FailMessage
 
-interface MemberRepository : JpaRepository<Member, Long> {
-    fun findByIdOrThrow(id: Long): Member {
-        return findById(id)
-            .orElseThrow { throw EntityNotFoundException(FailMessage.MEMBER_NOT_FOUND) }
-    }
-}
+interface MemberRepository : JpaRepository<Member, Long>
