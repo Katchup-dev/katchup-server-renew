7fac2e87969a2b7c695a9906dbdddfaf8305b1db
unanchoi <unanc2@gmail.com>
2024-05-06 14:53:37 +0900

    chore: FcmSender 생성
    FCM 푸시 알람 전송 로직은 이후에 구현

---
+++ src/main/kotlin/site/katchup/springboot/external/fcm/FcmSender.kt
@@ -0,0 +1,11 @@
+package site.katchup.springboot.external.fcm
+
+import org.springframework.stereotype.Component
+
+@Component
+class FcmSender {
+
+    // TODO: implement FCM sending logic
+    fun send() {
+    }
+}
