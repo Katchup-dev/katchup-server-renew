a78300acb46dd0072681e674d5bbcde67aa28edd
unanchoi <unanc2@gmail.com>
2024-05-07 01:14:24 +0900

    refactor: Task 필드 추가 및 validation 로직 추가

--- src/main/kotlin/site/katchup/springboot/entity/Task.kt
+++ src/main/kotlin/site/katchup/springboot/entity/Task.kt
@@ -4,14 +4,31 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import site.katchup.springboot.exception.entity.InvalidEntityArgumentException
+import site.katchup.springboot.global.message.FailMessage
 
 @Entity
 class Task(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    val title: String,
-    val content: String,
+    var title: String,
+    var content: String,
     val status: TaskStatus,
     val memberWorkSpaceId: Long,
-) : BaseTimeEntity()
+) : BaseTimeEntity() {
+
+    init {
+        validate()
+    }
+
+    private fun validate() {
+        if (title.length > 60) {
+            throw InvalidEntityArgumentException(FailMessage.INVALID_TASK_TITLE)
+        }
+
+        if (content.length > 600) {
+            throw InvalidEntityArgumentException(FailMessage.INVALID_TASK_CONTENT)
+        }
+    }
+}
